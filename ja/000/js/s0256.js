2a00428be4be22a3c51f092fdb669810f3934db7
Daichi1983 <77876382+Daichi1983@users.noreply.github.com>
2021-07-18 20:51:53 +0900

    temporary

--- ja/000/js/s0256.js
+++ ja/000/js/s0256.js
@@ -1,3 +1,3 @@
 ﻿document.write('<div class="container">');
-document.write('<div class="row"><div class="col-2">袋番号</div><div class="col-8">タイトル</div><div class="col-2">ページ</div></div>');
-document.write('<div class="row"><div class="col-2">83-1R</div><div class="col-8">in<br>（1）<u>auf</u> eine Formal <u>bringen</u>   特<br>（2）熟語化した   特<br>（3）結果のin  Etwas wird zu etwas.（IV.）<br>（4）転化終結のin（III）  Werden-"in"の三格支配<br>in etwas enden.<br>（5）los!  nut = ［in etwas ausbrechen, usw.］<br>本当は自働詞のみの部</div><div class="col-2">1-53</div></div></div>');
+document.write('<div class="row"><div class="col-2">袋番号</div><div class="col-8">タイトル</a></div><div class="col-2">ページ</div></div>');
+document.write('<div class="row"><div class="col-2">83-1R</div><div class="col-8">in<br>（1）<u>auf</u> eine Formal <u>bringen</u>   特<br>（2）熟語化した   特<br>（3）結果のin  Etwas wird zu etwas.（IV.）<br>（4）転化終結のin（III）  Werden-"in"の三格支配<br>in etwas enden.<br>（5）los!  nut = ［in etwas ausbrechen, usw.］<br>本当は自働詞のみの部</a></div><div class="col-2">1-53</div></div></div>');
