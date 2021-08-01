f50819bf65c0598b1efac293e138334c47dd5a08
Daichi1983 <77876382+Daichi1983@users.noreply.github.com>
2021-08-01 23:09:49 +0900

    eng version files completed

---
+++ en/015/breadcrumb.js
@@ -0,0 +1,7 @@
+﻿document.write('<nav aria-label="breadcrumb">');
+document.write('<ol class="breadcrumb">');
+document.write('<li class="breadcrumb-item"><a href="/">ホーム</a></li>');
+document.write('<li class="breadcrumb-item"><a href="../all-index.html">総目次</a></li>');
+document.write('<li class="breadcrumb-item active" aria-current="page">名詞</li>');
+document.write('</ol>');
+document.write('</nav>');
---
+++ en/088/js/js-format.py
@@ -0,0 +1,36 @@
+﻿import re
+import codecs
+import os
+
+place = os.getcwd()
+place_s = place.split("\\")
+folder = place_s[-2] #ディレクトリ名を得る
+
+fl = codecs.open("file_list.txt", "r")
+flr = fl.read()
+flr = flr.replace(".tif",".js")
+flr = flr.replace(".TIF",".js")
+flr = flr.replace("L0","l0")
+flr = flr.replace("S0","s0")
+flr = flr.split("\n")
+nmb = 0
+
+lnth = len(flr)
+del flr[-1]
+
+for i in flr:
+ nmp = nmb - 1
+ nma = nmb + 1
+ fl1 = codecs.open("sample.js", "r", encoding="utf-8")
+ main_content = fl1.read()
+ main_content = main_content.replace("番号",folder)
+ main_content = main_content.replace("置換",i[1:5])
+ print(main_content)
+ print(i[1:5])
+ fl2 = codecs.open(i, "w", encoding="utf-8")
+ fl2.write(main_content)
+ fl2.close()
+ nmb += 1
+
+print("Big Finish")
+fl.close()
